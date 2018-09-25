d4acd6902fb9ca4a8f53f755047e6a19f11eb015
devy.viray <demetrio.viray@lafilgroup.com>
2018-09-25 17:33:55 +0800

    UserTable seeder alignment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,13 +15,12 @@ class DatabaseSeeder extends Seeder
         //  $this->call(UsersTableSeeder::class);
         Model::unguard();
 
+            $this->call('UsersTableSeeder');
             $this->call('PermissionsTableSeeder');
             $this->call('RolesTableSeeder');
             $this->call('ConnectRelationshipsSeeder');
             $this->call('DepartmentsTableSeeder');
             $this->call('CompaniesTableSeeder');
-
-            $this->call('UsersTableSeeder');
 
         Model::reguard();
     }
