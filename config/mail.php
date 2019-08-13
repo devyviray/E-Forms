57a0136b892d41447c51a11488ea193ed81dbbc1
devy.viray <demetrio.viray@lafilgroup.com>
2019-08-13 15:22:45 +0800

    email server

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'QMD-adminp@lafilgroup.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'QMD-admin@mail.lafilgroup.com'),
         'name' => env('MAIL_FROM_NAME', 'QMD-EFORMS'),
     ],
 
