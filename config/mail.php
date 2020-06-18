ff3e51033e6c28d910b93d5e3aabe292b7576fbe
devy.viray <demetrio.viray@lafilgroup.com>
2020-06-18 10:15:01 +0800

    Email set up. mailgun to smtp

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'QMD-admin@mail.lafilgroup.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'qmd.eforms@lafilgroup.com'),
         'name' => env('MAIL_FROM_NAME', 'QMD-EFORMS'),
     ],
 
