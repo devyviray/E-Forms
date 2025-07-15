b8c5381d7525c3811008f5fa5a0a9e147c86e99a
Devy Viray <demetrio.viray@lfuggoc.net>
2025-07-15 15:45:56 +0800

    Fixed spelling of version release

--- database/migrations/2025_04_08_085543_version_releases_table.php
+++ database/migrations/2025_04_08_085543_version_releases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VersionReleaseTable extends Migration
+class VersionReleasesTable extends Migration
 {
     /**
      * Run the migrations.
