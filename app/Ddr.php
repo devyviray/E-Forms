83894276c47ebde4c49515ee371dbcec1fa2599f
Devy Viray <demetrio.viray@lfuggoc.net>
2025-12-15 14:29:33 +0800

    Implement soft delete in DDR

--- app/Ddr.php
+++ app/Ddr.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ddr extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+    use SoftDeletes;
     /**
      * The table associated with the model.
      *
